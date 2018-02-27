995a1da2dc8e73b37ee6daf53a4bfc3d8ec865e1
the0d3v <36508243+the0d3v@users.noreply.github.com>
2018-02-27 17:42:32 +0100

    v2-b1

---
+++ src/Editor_V2/Form1.Designer.vb
@@ -0,0 +1,144 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class editor_form
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.name = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(46, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "New"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Location = New System.Drawing.Point(12, 41)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(516, 261)
+        Me.TabControl1.TabIndex = 1
+        '
+        'name
+        '
+        Me.name.Location = New System.Drawing.Point(122, 15)
+        Me.name.Name = "name"
+        Me.name.Size = New System.Drawing.Size(109, 20)
+        Me.name.TabIndex = 2
+        Me.name.Text = "File Name"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(237, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(54, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "C#"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(296, 12)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(57, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "VB.NET"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(359, 12)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(47, 23)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "HTML"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(412, 12)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(54, 23)
+        Me.Button5.TabIndex = 6
+        Me.Button5.Text = "JS"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(472, 12)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(56, 23)
+        Me.Button6.TabIndex = 7
+        Me.Button6.Text = "Lua"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(64, 12)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(52, 23)
+        Me.Button7.TabIndex = 8
+        Me.Button7.Text = "Save"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'editor_form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(593, 314)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.name)
+        Me.Controls.Add(Me.TabControl1)
+        Me.Controls.Add(Me.Button1)
+        Me.Text = "Editor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents name As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+End Class
---
+++ src/Editor_V2/Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class editor_form
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim t As New TabPage : Dim n As New FastColoredTextBoxNS.FastColoredTextBox
+
+        t.Text = name.Text
+
+        n.Dock = DockStyle.Fill : t.Controls.Add(n)
+        Me.TabControl1.Controls.Add(t)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        CType(Me.TabControl1.SelectedTab.Controls.Item(0), FastColoredTextBoxNS.FastColoredTextBox).Language = FastColoredTextBoxNS.Language.CSharp
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        CType(Me.TabControl1.SelectedTab.Controls.Item(0), FastColoredTextBoxNS.FastColoredTextBox).Language = FastColoredTextBoxNS.Language.VB
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        CType(Me.TabControl1.SelectedTab.Controls.Item(0), FastColoredTextBoxNS.FastColoredTextBox).Language = FastColoredTextBoxNS.Language.HTML
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+
+    End Sub
+End Class
